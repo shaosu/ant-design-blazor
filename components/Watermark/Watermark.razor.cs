b8ba040a76fcdca3cad370cc187e30a8967e89be
James Yeung <shunjiey@hotmail.com>
2023-07-20 00:31:00 +0800

    feat(module: watermark): add Watermark component

---
+++ components/Watermark/Watermark.razor.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class Watermark : AntDomComponentBase
+    {
+        [Parameter] public string Width { get; set; } = "120px";
+
+        [Parameter] public string Height { get; set; } = "64px";
+
+        [Parameter] public int Rotate { get; set; } = -22;
+
+        [Parameter] public float Opacity { get; set; } = 0.5f;
+
+        [Parameter] public string FontSize { get; set; } = "14px";
+
+        [Parameter] public string FontColor { get; set; } = "rgba(0,0,0,.15)";
+
+        [Parameter] public string FontFamily { get; set; } = "sans-serif";
+
+        [Parameter] public string FontWeight { get; set; } = "normal";
+
+        [Parameter] public string FontStyle { get; set; } = "normal";
+
+        [Parameter] public string Content { get; set; }
+
+        [Parameter] public int ZIndex { get; set; } = 9;
+
+        [Parameter] public (int X, int Y) Gap { get; set; } = (100, 100);
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+
+        public string GetSvg()
+        {
+            var svgStr = $"""
+                  <svg xmlns="http://www.w3.org/2000/svg" width="{Width}" height="{Height}">
+                    <text x="50%" y="50%" dy="12px"
+                     text-anchor="middle"
+                     stroke="#000000"
+                     stroke-width="1"
+                     fill="none"
+                     font-weight="{FontWeight}"
+                     font-style="{FontStyle}"
+                     font-family="{FontFamily}"
+                     font-size="{FontSize}"
+                     style="
+                        transform: rotate({Rotate}deg);
+                        stroke: {FontColor};
+                     ">
+                     {HttpUtility.HtmlEncode(Content)}
+                    </text>
+                  </svg>
+                  """;
+
+            var base64Url = $"data:image/svg+xml;base64,{Convert.ToBase64String(Encoding.Default.GetBytes(svgStr))}";
+
+            var styleStr = $"""
+                    z-index: {ZIndex};
+                    pointer-events:none;
+                    background-repeat:repeat;
+                    background-image:url('{base64Url}');
+                    """;
+
+            return styleStr;
+        }
+    }
+}
