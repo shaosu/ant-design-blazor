fe4919a79ef308e1aec2740fa0de6446722eff71
James Yeung <shunjiey@hotmail.com>
2023-09-08 00:21:52 +0800

    fix expand

--- components/tree/Tree.razor.cs
+++ components/tree/Tree.razor.cs
@@ -679,12 +679,12 @@ namespace AntDesign
             return base.OnFirstAfterRenderAsync();
         }
 
-        protected override void OnParametersSet()
+        public override async Task SetParametersAsync(ParameterView parameters)
         {
-            UpdateState();
-            base.OnParametersSet();
-        }
+            await base.SetParametersAsync(parameters);
 
+            UpdateState();
+        }
         /// <summary>
         /// Get TreeNode from Key
         /// </summary>
@@ -782,7 +782,7 @@ namespace AntDesign
 
         internal async Task OnNodeExpand(TreeNode<TItem> node, bool expanded, MouseEventArgs args)
         {
-            var expandedKeys = _allNodes.Where(x=>x.Expanded).Select(x => x.Key).ToArray();
+            var expandedKeys = _allNodes.Where(x => x.Expanded).Select(x => x.Key).ToArray();
             if (OnNodeLoadDelayAsync.HasDelegate && expanded == true)
             {
                 node.SetLoading(true);
@@ -860,12 +860,11 @@ namespace AntDesign
 
         private void UpdateState()
         {
-
-            foreach(var node in _allNodes)
+            foreach (var node in _allNodes)
             {
-                node.SetChecked(CheckedKeys?.Contains( node.Key)==true);
-                node.SetSelected(SelectedKeys?.Contains(node.Key)==true);
-                Switch(node, ExpandedKeys?.Contains(node.Key) == true);
+                node.SetChecked(CheckedKeys?.Contains(node.Key) == true);
+                node.SetSelected(SelectedKeys?.Contains(node.Key) == true);
+                node.Expand(ExpandedKeys?.Contains(node.Key) == true);
             }
         }
     }
